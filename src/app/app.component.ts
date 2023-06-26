f17c9ba53fa9b5557fcb595df6253e35102e329d
qurriahSam <mugirethe1@gmail.com>
2023-06-26 17:35:05 +0300

    feat: get todos from todoService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { TodoService } from './service/todo.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo';
+  todos: Todo[] = [];
+  constructor(private _todoService: TodoService) {}
+
+  ngOnInit() {
+    this._todoService.todos$.subscribe((todos) => {
+      this.todos = todos;
+    });
+  }
+
+  addTodo(newTodo: Todo) {
+    this.todos.push(newTodo);
+  }
 }
