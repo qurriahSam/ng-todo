2ad988ec7ad28154586af584d030f162bc577e5d
qurriahSam <mugirethe1@gmail.com>
2023-06-26 17:39:07 +0300

    feat: add new todoo to service and output to parent

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+import { TodoService } from 'src/app/service/todo.service';
+import { Todo } from 'src/app/todo';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.scss']
+  styleUrls: ['./add-todo.component.scss'],
 })
 export class AddTodoComponent {
+  @Output() newTodoItemEvent = new EventEmitter<Todo>();
+  todo: Todo = { task: '', completed: false };
 
+  constructor(private _todoService: TodoService) {}
+
+  addTask(todo: Todo) {
+    this._todoService.addTodo(todo);
+    console.log(todo);
+    this.addNewTodo(todo);
+    //    this.todo.task = '';
+  }
+
+  addNewTodo(todo: Todo) {
+    this.newTodoItemEvent.emit(todo);
+  }
 }
